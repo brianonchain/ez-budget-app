1296c7d0adb5283df565e776ee5bb498efe12f7a
Brian Huang <jonwayhuang@gmail.com>
2026-02-09 23:49:38 +0700

    add manifest.ts

---
+++ frontend/src/manifest.ts
@@ -0,0 +1,24 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "EZ Budget App",
+    short_name: "EZ Budget",
+    start_url: "/app/items",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#ffffff",
+    icons: [
+      {
+        src: "/public/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/public/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
