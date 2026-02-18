b7cc27183c09ddedfe33cd8137736c2f04069c0e
Brian Huang <jonwayhuang@gmail.com>
2026-02-18 20:21:25 +0800

    change

--- frontend/src/utils/components/Modal.tsx
+++ frontend/src/utils/components/Modal.tsx
@@ -1,3 +1,5 @@
+import { useId } from "react";
+
 export default function Modal({
   children,
   disableCloseButton,
@@ -9,23 +11,33 @@ export default function Modal({
   setIsOpen: (isOpen: boolean) => void;
   title: string;
 }) {
+  const titleId = useId();
+
   return (
-    <div>
-      <div className="modalFull">
+    <div className="fixed inset-0 z-[50]">
+      <div className="fixed inset-0 bg-black/70 z-[0]"></div>
+      <div
+        className="modalFullColor modalFullSize desktop:pb-[12px] absolute z-[1] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col desktop:rounded-2xl overflow-hidden"
+        role="dialog"
+        aria-modal="true"
+        aria-labelledby={titleId}
+        tabIndex={-1} // so modal is focusable with javascript
+      >
         {/*--- glow ---*/}
-        <div className="absolute w-full h-full left-0 top-0 bg-gradient-to-br from-lightBg1 to-lightBg1 dark:from-blue-500/20 dark:to-blue-500/10 z-[-1]"></div>
+        <div className="absolute inset-0 bg-gradient-to-br from-lightBg1 to-lightBg1 dark:from-blue-500/20 dark:to-blue-500/10 z-[-1]"></div>
         {/*--- close ---*/}
         {!disableCloseButton && (
-          <button className="xButton" onClick={() => setIsOpen(false)}>
+          <button className="xButton" aria-label="Close" onClick={() => setIsOpen(false)}>
             &#10005;
           </button>
         )}
         {/*--- title ---*/}
-        <div className="modalFullHeader">{title}</div>
+        <div id={titleId} className="modalFullHeader">
+          {title}
+        </div>
         {/*--- content ---*/}
         <div className="modalFullContentContainer">{children}</div>
       </div>
-      <div className="modalBlackout"></div>
     </div>
   );
 }
