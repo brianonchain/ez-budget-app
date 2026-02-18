2f34eaf78b543f29c89019dacd419c0f34c60647
Brian Huang <jonwayhuang@gmail.com>
2026-02-18 21:02:45 +0800

    manifest change

--- frontend/src/manifest.ts
+++ frontend/src/manifest.ts
@@ -4,18 +4,18 @@ export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "EZ Budget App",
     short_name: "EZ Budget",
-    start_url: "https://www.ezbudgetapp.com/app/items",
+    start_url: "/app/items",
     display: "standalone",
     background_color: "#ffffff",
     theme_color: "#ffffff",
     icons: [
       {
-        src: "/public/icon-192.png",
+        src: "/icon-192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/public/icon-512.png",
+        src: "/icon-512.png",
         sizes: "512x512",
         type: "image/png",
       },
