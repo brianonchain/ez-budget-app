bb3b9b8bd565b28c39d7d44862381bedb4dbc155
Brian Huang <jonwayhuang@gmail.com>
2026-02-21 20:03:57 +0800

    changes

--- frontend/src/app/(app)/app/settings/Settings.tsx
+++ frontend/src/app/(app)/app/settings/Settings.tsx
@@ -146,9 +146,9 @@ export default function Settings({ provider, email }: { provider: string; email:
       {addTagModal && <AddTagModal setAddTagModal={setAddTagModal} data={data} />}
 
       {/*--- glow ---*/}
-      <div className="absolute w-full h-full left-0 top-0 z-[-1]">
+      {/* <div className="absolute w-full h-full left-0 top-0 z-[-1]">
         <div className="absolute top-1/2 right-0 translate-y-[-50%] translate-x-[50%] w-[90%] h-[50%] rounded-full bg-white dark:bg-[#0444B7] blur-[200px] portrait:sm:dark:blur-[300px] landscape:lg:blur-[300px] pointer-events-none"></div>
-      </div>
+      </div> */}
     </div>
   );
 }
