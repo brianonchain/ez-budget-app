44c670de77356c3ddedabb6c7c33a55dfe454d6c
Brian Huang <jonwayhuang@gmail.com>
2026-03-27 15:16:32 +0800

    landing text

--- frontend/src/app/(landing)/layout.tsx
+++ frontend/src/app/(landing)/layout.tsx
@@ -1,3 +1,3 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="dark bg-primaryBg text-lg">{children}</div>;
+  return <div className="dark bg-primaryBg text-textPrimary text-lg">{children}</div>;
 }
