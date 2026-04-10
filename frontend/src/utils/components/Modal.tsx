2a96f068886082542045d8147d64f4cbef711c72
Brian Huang <jonwayhuang@gmail.com>
2026-04-11 01:54:11 +0800

    edit modals

--- frontend/src/utils/components/Modal.tsx
+++ frontend/src/utils/components/Modal.tsx
@@ -70,8 +70,8 @@ export default function Modal({
 
           {/*--- content ---*/}
           <div className="flex-1 min-h-0 overflow-y-auto pt-6 desktop:pt-2 pb-12 desktop:pb-8 px-4 tablet:px-8 desktop:px-10 w-full thinScrollbar">
-            {/*--- this inner needed to limit content width in mobile/tablet (full screen mode), while desktop:max-w-104 (above) desktop content width  ---*/}
-            <div className="mx-auto w-full max-w-100 desktop:max-w-none flex flex-col">{children}</div>
+            {/*--- max-w here mainly defines mobile/tablet content width  ---*/}
+            <div className={`mx-auto w-full max-w-100 ${contentMaxWidth} desktop:max-w-none flex flex-col`}>{children}</div>
           </div>
         </div>
       </FocusTrap>
